6b4e203b56d5a825dc5be7f5fcedf9580c46f198
laurennelsen <77991089+laurennelsen@users.noreply.github.com>
2025-03-31 03:26:27 +0000

    Latest build deployed.

---
+++ lunr-pretext-search-index.js
@@ -0,0 +1,158 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "frontmatter-2",
+  "level": "1",
+  "url": "frontmatter-2.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": "  My Website   copyright  "
+},
+{
+  "id": "sec-analyzing-Pixar-films-Excel",
+  "level": "1",
+  "url": "sec-analyzing-Pixar-films-Excel.html",
+  "type": "Section",
+  "number": "1.1",
+  "title": "Analyzing Pixar film data in Excel",
+  "body": " Analyzing Pixar film data in Excel   Use this link to open an Excel file in Office 365: Pixar Film Data   Go to File and create a copy of this file online:    Click on the Copilot icon so that a Copilot window shows up on your screen:     Now let's use Copilot to investigate our data!      What film did the best (or the worst) in the United States and Canada?    Sort the films from largest to smallest using worlwide box office sales [insert column].    Highlight any movies that had a worldwide sales amount that is less than the average    Create a column for profit...      Now let's practice using Copilot in Excel to create data visualizations.      Create a scatter plot for budget vs. worldwide sales.    Create a line chart showing the number of films released each year.    Make a table showing how many films were rated G and PG.    How did average profit for G movies compare to average profit for PG movies?    Make a boxplot for the Rotten Tomatoes Score. Are there any outliers?         Create a confidence interval for the mean box_office_worldwide in column E.    Create a regression model to predict box_office_worldwide based on budget.    Is there an error? Why?    If you are able to use the response to create the model, how could you verify that it is correct?       "
+},
+{
+  "id": "sec-analyzing-Pixar-films-Excel-4",
+  "level": "2",
+  "url": "sec-analyzing-Pixar-films-Excel.html#sec-analyzing-Pixar-films-Excel-4",
+  "type": "Example",
+  "number": "1.1.1",
+  "title": "",
+  "body": "    What film did the best (or the worst) in the United States and Canada?    Sort the films from largest to smallest using worlwide box office sales [insert column].    Highlight any movies that had a worldwide sales amount that is less than the average    Create a column for profit...     "
+},
+{
+  "id": "sec-analyzing-Pixar-films-Excel-6",
+  "level": "2",
+  "url": "sec-analyzing-Pixar-films-Excel.html#sec-analyzing-Pixar-films-Excel-6",
+  "type": "Example",
+  "number": "1.1.2",
+  "title": "",
+  "body": "    Create a scatter plot for budget vs. worldwide sales.    Create a line chart showing the number of films released each year.    Make a table showing how many films were rated G and PG.    How did average profit for G movies compare to average profit for PG movies?    Make a boxplot for the Rotten Tomatoes Score. Are there any outliers?     "
+},
+{
+  "id": "sec-analyzing-Pixar-films-Excel-7",
+  "level": "2",
+  "url": "sec-analyzing-Pixar-films-Excel.html#sec-analyzing-Pixar-films-Excel-7",
+  "type": "Example",
+  "number": "1.1.3",
+  "title": "",
+  "body": "   Create a confidence interval for the mean box_office_worldwide in column E.    Create a regression model to predict box_office_worldwide based on budget.    Is there an error? Why?    If you are able to use the response to create the model, how could you verify that it is correct?      "
+},
+{
+  "id": "sec-using-Copilot-to-learn-how",
+  "level": "1",
+  "url": "sec-using-Copilot-to-learn-how.html",
+  "type": "Section",
+  "number": "1.2",
+  "title": "Using Copilot to Learn How to do Things in Excel",
+  "body": " Using Copilot to Learn How to do Things in Excel   At the moment, Copilot in Excel cannot do everything we might want it to do. However, it can still be helpful for providing guidance if we are not sure how to do something in Excel.  Use this link to open an Excel file in Office 365: Dentists by state  This data is from .   Go to File and create a copy of this file online.    Dentist Geographic Heat Map  We want to create a geographic heat map showing the number of dentists per 100,000 residents in each state in the U.S. in 2021.     Ask Copilot to create a geographic heat map showing the number of dentists per 100,000 residents in each state in the U.S. in 2021.  Did it create a heat map?    Copilot probably did not create a heat map. Let's ask it how we would do it ourselves using the prompt below.   How do I create a geographic heat map for the number of dentists per 100,000 resident population in each state in 2021?   Follow the isntructions it gives to create the heat map.      "
+},
+{
+  "id": "sec-using-Copilot-to-learn-how-3",
+  "level": "2",
+  "url": "sec-using-Copilot-to-learn-how.html#sec-using-Copilot-to-learn-how-3",
+  "type": "Example",
+  "number": "1.2.1",
+  "title": "Dentist Geographic Heat Map.",
+  "body": " Dentist Geographic Heat Map  We want to create a geographic heat map showing the number of dentists per 100,000 residents in each state in the U.S. in 2021.     Ask Copilot to create a geographic heat map showing the number of dentists per 100,000 residents in each state in the U.S. in 2021.  Did it create a heat map?    Copilot probably did not create a heat map. Let's ask it how we would do it ourselves using the prompt below.   How do I create a geographic heat map for the number of dentists per 100,000 resident population in each state in 2021?   Follow the isntructions it gives to create the heat map.     "
+},
+{
+  "id": "sec-describe-visualization",
+  "level": "1",
+  "url": "sec-describe-visualization.html",
+  "type": "Section",
+  "number": "2.1",
+  "title": "Using Generative AI to Describe a Data Visualization",
+  "body": " Using Generative AI to Describe a Data Visualization   Choose (or create) a visualization of data that you want to describe.  Use either ChatGPT or Copilot to generate a description of the image. Think about the following questions after generating the description:   (Is the image you used misleading in any way?)    Is the description accurate?    Is there anything notable that the generated description does not mention\/include?    Does the description make sense?     If you aren't sure what image to use, here are some that you can try:         This image is from the budgeting software Monarch .       "
+},
+{
+  "id": "sec-describe-visualization-2",
+  "level": "2",
+  "url": "sec-describe-visualization.html#sec-describe-visualization-2",
+  "type": "Example",
+  "number": "2.1.1",
+  "title": "",
+  "body": " Choose (or create) a visualization of data that you want to describe.  Use either ChatGPT or Copilot to generate a description of the image. Think about the following questions after generating the description:   (Is the image you used misleading in any way?)    Is the description accurate?    Is there anything notable that the generated description does not mention\/include?    Does the description make sense?     If you aren't sure what image to use, here are some that you can try:         This image is from the budgeting software Monarch .      "
+},
+{
+  "id": "sec-create-visualization",
+  "level": "1",
+  "url": "sec-create-visualization.html",
+  "type": "Section",
+  "number": "2.2",
+  "title": "Using Generative AI to Create Data Visualizations",
+  "body": " Using Generative AI to Create Data Visualizations   The analysis we can do and the visualizations we can create often depend on what version of a particular chatbot we are using. Some won't do much analysis with the free versions. (ChatGPT currently has Python incorporated into it and can do some of the analysis\/creating of visualizations using Python, but the free version is limited in what it will do in one day.)  In the examples below, think about what visualization would make sense to create to visualize some aspect(s) of the dataset.  Use either ChatGPT or Copilot to try to create that visualization. Think about the following questions as you view any images\/charts it creates:   Is the chart misleading in any way?    Is the chart accurate?    Is there anything notable that the generated chart\/image does not include?    Does the chart make sense? (Are axes labeled\/is it clear what is being shown?)        US Home Sales  The file NewHouseSalesUS.xls contains data for new, privately-owned, residential housing units sold and for sale in the United States between 2002 to the present.     (This file can also be found at .)   Here are ideas for visualizations you can create:   Make a histogram showing the number of homes sold in each price range in a given year.    Make a line chart showing how the number of homes in the to price range has changed from 2002 to 2022.        Colorado Childcare Data  Download the file below that has data on licensed childcare facilities in Colorado:   We're going to use this file to create a geographic heat map in Tableau.    Maddison Project Database   The Maddison Project Database provides information on comparative economic growth and income levels over the very long run.   Use the link below to download the Maddison Project Database as an Excel file for the period up to 2022:     Think about what visualizations would make sense to create with this data and use Tableau to create visualizations for a few different aspects of the data.    Domestic Box Office  Download the file below showing box office totals for new movie releases starting in 2022 through mid-September 2024.     Create at least one visualization showing some aspect of this data.    Choose a larger dataset that you would like to analyze. Try to use either ChatGPT or Copilot to create a visualization of the data.  Here are links to sites with datasets that you could use:    Data is Plural      The English Women's Football (EWF) Database       "
+},
+{
+  "id": "sec-create-visualization-3",
+  "level": "2",
+  "url": "sec-create-visualization.html#sec-create-visualization-3",
+  "type": "Example",
+  "number": "2.2.1",
+  "title": "US Home Sales.",
+  "body": " US Home Sales  The file NewHouseSalesUS.xls contains data for new, privately-owned, residential housing units sold and for sale in the United States between 2002 to the present.     (This file can also be found at .)   Here are ideas for visualizations you can create:   Make a histogram showing the number of homes sold in each price range in a given year.    Make a line chart showing how the number of homes in the to price range has changed from 2002 to 2022.      "
+},
+{
+  "id": "sec-create-visualization-4",
+  "level": "2",
+  "url": "sec-create-visualization.html#sec-create-visualization-4",
+  "type": "Example",
+  "number": "2.2.2",
+  "title": "Colorado Childcare Data.",
+  "body": " Colorado Childcare Data  Download the file below that has data on licensed childcare facilities in Colorado:   We're going to use this file to create a geographic heat map in Tableau.  "
+},
+{
+  "id": "sec-create-visualization-5",
+  "level": "2",
+  "url": "sec-create-visualization.html#sec-create-visualization-5",
+  "type": "Example",
+  "number": "2.2.3",
+  "title": "Maddison Project Database.",
+  "body": " Maddison Project Database   The Maddison Project Database provides information on comparative economic growth and income levels over the very long run.   Use the link below to download the Maddison Project Database as an Excel file for the period up to 2022:     Think about what visualizations would make sense to create with this data and use Tableau to create visualizations for a few different aspects of the data.  "
+},
+{
+  "id": "sec-create-visualization-6",
+  "level": "2",
+  "url": "sec-create-visualization.html#sec-create-visualization-6",
+  "type": "Example",
+  "number": "2.2.4",
+  "title": "Domestic Box Office.",
+  "body": " Domestic Box Office  Download the file below showing box office totals for new movie releases starting in 2022 through mid-September 2024.     Create at least one visualization showing some aspect of this data.  "
+},
+{
+  "id": "sec-create-visualization-7",
+  "level": "2",
+  "url": "sec-create-visualization.html#sec-create-visualization-7",
+  "type": "Example",
+  "number": "2.2.5",
+  "title": "",
+  "body": " Choose a larger dataset that you would like to analyze. Try to use either ChatGPT or Copilot to create a visualization of the data.  Here are links to sites with datasets that you could use:    Data is Plural      The English Women's Football (EWF) Database      "
+},
+{
+  "id": "backmatter-2",
+  "level": "1",
+  "url": "backmatter-2.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": " This book was authored in PreTeXt .  "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+  this.metadataWhitelist = ['position']
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
